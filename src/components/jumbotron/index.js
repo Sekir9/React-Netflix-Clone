cf5e092ee0b004d1defe866cbb09eb09d53f6689
Sebatian Cizla <sebastiann.cizla@gmail.com>
2022-01-16 22:50:42 +0100

    added images and fixtures

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,31 @@
+import { Container, Image, Inner, Item, Pane, SubTitle, Title } from './styles/jumbotron';
+
+import React from 'react';
+
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <Inner {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Inner>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
