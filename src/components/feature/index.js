3eeb295d99d15a2696a2bf9feb1740f01f9d63b9
Sebatian Cizla <sebastiann.cizla@gmail.com>
2022-02-06 13:15:05 +0100

    Working sign in page

--- src/components/feature/index.js
+++ src/components/feature/index.js
@@ -3,13 +3,12 @@ import { Container, SubTitle, Title } from './styles/feature';
 import React from "react";
 
 export default function Feature({ children, ...restProps }) {
-    return <Container {...restProps}> </Container>
+    return <Container {...restProps}>{children}</Container>
 }
 
 Feature.Title = function FeatureTitle({children, ...restProps}) {
     return <Title {...restProps}>{children}</Title>
 }
-
 
 Feature.SubTitle = function FeatureSubTitle({children, ...restProps}) {
     return <SubTitle {...restProps}>{children}</SubTitle>
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,40 +1,52 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
 
-import { FooterContainer } from "../containers/footer";
-import { Form } from "../components";
-import { HeaderContainer } from "../containers/header";
 
-export default function Signin() {
-  const [emailAdress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
-  const [error, setError] = useState("");
+export default function SignIn() {
 
-  const isInvalid = password === '' || emailAdress === '';
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
       <HeaderContainer>
         <Form>
           <Form.Title>Sign In</Form.Title>
-          {error && <Form.Error>{error}</Form.Error>}
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
 
           <Form.Base onSubmit={handleSignin} method="POST">
             <Form.Input
               placeholder="Email address"
-              value={emailAdress}
+              value={emailAddress}
               onChange={({ target }) => setEmailAddress(target.value)}
             />
             <Form.Input
               type="password"
-              placeholder="Password"
-              autoComplete="off"
               value={password}
+              autoComplete="off"
+              placeholder="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
-                Sign In
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
+              Sign In
             </Form.Submit>
           </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
         </Form>
       </HeaderContainer>
       <FooterContainer />
